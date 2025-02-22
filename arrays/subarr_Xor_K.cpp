a57039e4b2539bb9c69fae3cad614356d00d59ec
tejesh-vempuluru <tejesh_vempuluru@outlook.com>
2025-02-22 16:58:37 +0530

    Count Subarray with Xor as K

---
+++ arrays/subarr_Xor_K.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Xor_K(vector<int>a,int k){
+    int xr=0;
+    map<int,int>mpp;
+    mpp[xr]++;
+    int cnt=0;
+    for(int i=0;i<a.size();i++){
+        xr=xr^a[i];
+        int x=xr^k;
+        cnt+=mpp[x];
+        mpp[xr]++;
+    }
+    return cnt;
+}
